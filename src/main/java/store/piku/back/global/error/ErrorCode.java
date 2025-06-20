47583e66b1e0a6e46879cab7645224fe432960a4
apfp77 <apfp77@naver.com>
2025-06-20 16:35:57 +0900

    feat: 배포환경 에러 시 디스코드 알림 추가

--- src/main/java/store/piku/back/global/error/ErrorCode.java
+++ src/main/java/store/piku/back/global/error/ErrorCode.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    DIARY_NOT_FOUND(404, "해당 일기 목록이 존재하지 않습니다.");
+    DIARY_NOT_FOUND(404, "해당 일기 목록이 존재하지 않습니다."),
 
-    private final int code;
+    INTERNAL_SERVER_ERROR(500, "서버에 오류가 발생했습니다.");
+
+    private final int status;
     private final String message;
 }
