f3335e9b83f1877ad183f2aaf3f84200e9d73e78
hdn1212 <gkseksk0119@naver.com>
2025-06-24 11:00:29 +0900

    일기 상세조회 기능 (#14)
    * 일기 조회
    
    * 일기 조회 완료

--- src/main/java/store/piku/back/diary/dto/ResponseDTO.java
+++ src/main/java/store/piku/back/diary/dto/ResponseDTO.java
@@ -8,12 +8,17 @@ import store.piku.back.diary.enums.Status;
 import java.time.LocalDate;
 import java.util.List;
 
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseDTO {
+    private Long diaryId;
     private Status status;
     private String content;
-    private List<String> photos;  // 업로드용
+    private List<String> imgUrls;  // 업로드용
     private LocalDate date;
+    private String nickname; // 작성자 닉네임 추가
+    private String avatar;
+
 }
