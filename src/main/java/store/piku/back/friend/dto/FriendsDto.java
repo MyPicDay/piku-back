0436007733f61ea63055242be82fead25b33ae14
hdn1212 <gkseksk0119@naver.com>
2025-07-01 16:33:20 +0900

    refactor : 스웨거 추가

--- src/main/java/store/piku/back/friend/dto/FriendsDto.java
+++ src/main/java/store/piku/back/friend/dto/FriendsDto.java
@@ -1,5 +1,6 @@
 package store.piku.back.friend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,13 +8,12 @@ import lombok.Data;
 @Data
 public class FriendsDto {
 
-     // id
-     private String userId;
+    @Schema(description = "친구의 사용자 ID", example = "friend123")
+    private String userId;
 
-    // 닉네임
-     private String nickname;
+    @Schema(description = "친구의 닉네임", example = "개발자친구")
+    private String nickname;
 
-    // 아바타
-     private String avatar;
-
+    @Schema(description = "친구의 아바타 URL 또는 이미지 경로", example = "https://example.com/avatar.jpg")
+    private String avatar;
 }
