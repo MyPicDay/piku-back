860132adbd155960589889fbb1456f2255426af9
hdn1212 <gkseksk0119@naver.com>
2025-07-09 22:03:50 +0900

    일기 추천 수정 (#69)
    Co-authored-by: apfp77 <78768234+apfp77@users.noreply.github.com>

--- src/main/java/store/piku/back/friend/entity/Friend.java
+++ src/main/java/store/piku/back/friend/entity/Friend.java
@@ -2,15 +2,16 @@ package store.piku.back.friend.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import store.piku.back.friend.key.FriendID;
 
 @IdClass(FriendID.class)
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "friend")
 @Getter
 public class Friend {
@@ -22,4 +23,13 @@ public class Friend {
     @Id
     @Column(name = "user_id_2")
     private String userId2;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private String createdAt;
+
+    public Friend(String userId1, String userId2) {
+        this.userId1 = userId1;
+        this.userId2 = userId2;
+    }
 }
