c1ec67dd61949f739fa1c359842ce7e160abc049
eilsamo600 <eilsamo600@gmail.com>
2025-07-10 17:27:35 +0900

    merge UserService

--- src/main/java/store/piku/back/user/service/UserService.java
+++ src/main/java/store/piku/back/user/service/UserService.java
@@ -2,6 +2,12 @@ package store.piku.back.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import store.piku.back.friend.dto.FriendsDTO;
+import store.piku.back.global.dto.RequestMetaInfo;
+import store.piku.back.global.util.ImagePathToUrlConverter;
+import store.piku.back.user.entity.User;
 import store.piku.back.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +18,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final ImagePathToUrlConverter imagePathToUrlConverter;
 
+    public Page<FriendsDTO> searchByName(String keyword, Pageable pageable, RequestMetaInfo requestMetaInfo) {
+
+        String formattedKeyword = "%" + keyword + "%";
+        Page<User> users = userRepository.searchByName(formattedKeyword, pageable);
+
+        return users.map(user -> {
+            String avatarUrl = imagePathToUrlConverter.userAvatarImageUrl(user.getAvatar(), requestMetaInfo);
+            return new FriendsDTO(user.getId(), user.getNickname(), avatarUrl);
+        });
+
+    }
 }
