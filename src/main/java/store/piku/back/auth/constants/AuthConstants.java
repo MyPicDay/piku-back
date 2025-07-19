3827da7e1ab7ff63c23b5a33c1f50cddf29dcd33
apfp77 <apfp77@naver.com>
2025-07-19 12:15:46 +0900

    feat: 토큰 시간 변경

--- src/main/java/store/piku/back/auth/constants/AuthConstants.java
+++ src/main/java/store/piku/back/auth/constants/AuthConstants.java
@@ -2,8 +2,8 @@ package store.piku.back.auth.constants;
 
 public class AuthConstants {
     public static final String REFRESH_TOKEN = "rn";
-    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 60 * 30; // 30 minutes
-    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 24 * 7; // 7 days
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 1000L * 60 * 30; // 30 minutes
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 1000L * 60 * 60 * 24 * 7; // 7 days
     public static final String BEARER_PREFIX = "Bearer ";
     public static final String DEVICE_ID_HEADER = "Device-Id";
 }
