79e3d4fa00b6ce41ac7ddd2704c347f5ff842c76
Hwang Chaeyeon <eilsamo600@gmail.com>
2025-07-22 19:43:00 +0900

    삭제된 댓글 반환 수정 (#101)
    Co-authored-by: apfp77 <78768234+apfp77@users.noreply.github.com>

--- src/main/java/store/piku/back/comment/repository/CommentRepository.java
+++ src/main/java/store/piku/back/comment/repository/CommentRepository.java
@@ -12,14 +12,16 @@ import store.piku.back.comment.entity.Comment;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @EntityGraph(attributePaths = "user")
-    Page<Comment> findByDiaryIdAndParentIsNull(Long diaryId, Pageable pageable);
-
-    int countByParentId(Long parentId);
-
-    @EntityGraph(attributePaths = "user")
-    Page<Comment> findByParentId(Long parentId, Pageable pageable);
+    Page<Comment> findByParentIdAndDeletedAtIsNull(Long parentId, Pageable pageable);
 
     @Query("SELECT COUNT(c) FROM Comment c WHERE c.diary.id = :diaryId")
     long countAllByDiaryId(@Param("diaryId") Long diaryId);
+
+    int countByParentIdAndDeletedAtIsNull(Long parentId);
+
+
+    @Query("SELECT c FROM Comment c WHERE c.diary.id = :diaryId AND c.parent IS NULL AND " +
+            "(c.deletedAt IS NULL OR EXISTS (SELECT r FROM Comment r WHERE r.parent = c AND r.deletedAt IS NULL))")
+    Page<Comment> findVisibleRootCommentsByDiaryId(@Param("diaryId") Long diaryId, Pageable pageable);
 
 }
