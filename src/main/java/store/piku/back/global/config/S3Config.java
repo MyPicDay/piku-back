b47febdb2dc95ae59895f56769691e82392fd6ce
apfp77 <apfp77@naver.com>
2025-07-23 07:04:18 +0900

    feat: 개발환경에서 S3client 설정 변경

--- src/main/java/store/piku/back/global/config/S3Config.java
+++ src/main/java/store/piku/back/global/config/S3Config.java
@@ -2,9 +2,15 @@ package store.piku.back.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import store.piku.back.diary.service.StorageProperties;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -15,14 +21,34 @@ public class S3Config {
         this.storageProperties = storageProperties;
     }
 
-    @Bean
-    public S3Client s3Client() {
+    @Bean(name = "s3Client")
+    @Profile("prod")
+    public S3Client s3ClientProd() {
         return S3Client.builder()
                 .region(Region.of(storageProperties.getRegion()))
-                // ec2에 직접 할당
-                // .credentialsProvider(
-                //         StaticCredentialsProvider.create(AwsBasicCredentials.create(storageProperties.getAccessKey(), storageProperties.getSecretKey()))
-                // )
+                // ec2에 역할 설정
+                .build();
+    }
+
+    @Bean(name = "s3Client")
+    @Profile("dev")
+    public S3Client s3ClientDev() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(storageProperties.getEndpoint())) // MinIO 서버 주소
+                .region(Region.of(storageProperties.getRegion()))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(
+                                        storageProperties.getAccessKey(),
+                                        storageProperties.getSecretKey()
+                                )
+                        )
+                )
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true) // 주소 기반 접근 설정
+                                .build()
+                )
                 .build();
     }
 }
