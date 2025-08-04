5b29a46d6bb92e7903a756b5a120b1416c3b07c7
hdn1212 <gkseksk0119@naver.com>
2025-08-04 17:06:20 +0900

    feat: dto 스웨거 추가

--- src/main/java/store/piku/back/notification/dto/response/NotificationResponseDTO.java
+++ src/main/java/store/piku/back/notification/dto/response/NotificationResponseDTO.java
@@ -19,16 +19,21 @@ public class NotificationResponseDTO {
     @Schema(description = "알림 메세지")
     private String message;
 
+    @Schema(description = "보낸 사람 닉네임")
     private String nickname;
 
+    @Schema(description = "보낸 사람 프로필 사진")
     private String avatarUrl;
 
+    @Schema(description = "알림 유형")
     private NotificationType type;
 
     @Schema(description = "해당 알림 일기 ID")
     private Long relatedDiaryId;
 
+    @Schema(description = "해당 일기 대표 사진")
     private String thumbnailUrl;
 
+    @Schema(description = "읽음 여부")
     private Boolean isRead;
 }
--- src/main/java/store/piku/back/notification/dto/response/SseResponse.java
+++ src/main/java/store/piku/back/notification/dto/response/SseResponse.java
@@ -1,5 +1,6 @@
 package store.piku.back.notification.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +12,25 @@ import store.piku.back.notification.entity.NotificationType;
 @NoArgsConstructor
 public class SseResponse {
 
+    @Schema(description = "알림 유형")
     private NotificationType type;
+
+    @Schema(description = "알림 메세지")
     private String message;
+
+    @Schema(description = "일기 ID")
     private Long relatedId;
+
+    @Schema(description = "보낸 사람 ID")
     private String senderId;
+
+    @Schema(description = "보낸 사람 닉네임")
     private String senderNickname;
+
+    @Schema(description = "보낸사람 프로필 사진")
     private String senderAvatarUrl;
+
+    @Schema(description = "해당 일기 대표 사진")
     private String thumbnailUrl;
 
 }
