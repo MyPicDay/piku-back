43d1e66922eb53d956176d32ec69f33bdce98654
hdn1212 <gkseksk0119@naver.com>
2025-08-04 10:12:39 +0900

    chore: merge

--- src/main/java/store/piku/back/diary/service/DiaryService.java
+++ src/main/java/store/piku/back/diary/service/DiaryService.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import store.piku.back.ai.entity.DiaryImageGeneration;
 import store.piku.back.ai.repository.DiaryImageGenerationRepository;
 import store.piku.back.ai.service.DiaryImageGenerationService;
+import store.piku.back.comment.service.CommentService;
 import store.piku.back.diary.dto.*;
 import store.piku.back.diary.entity.Diary;
 import store.piku.back.diary.entity.Photo;
@@ -26,6 +27,8 @@ import store.piku.back.file.FileUtil;
 import store.piku.back.friend.service.FriendRequestService;
 import store.piku.back.global.dto.RequestMetaInfo;
 import store.piku.back.global.util.ImagePathToUrlConverter;
+import store.piku.back.notification.entity.NotificationType;
+import store.piku.back.notification.service.NotificationService;
 import store.piku.back.user.entity.User;
 import store.piku.back.user.exception.UserNotFoundException;
 import store.piku.back.user.service.reader.UserReader;
@@ -54,6 +57,9 @@ public class DiaryService {
     private final FileUtil fileUtil;
     private final DiaryImageGenerationRepository diaryImageGenerationRepository;
     private final DiaryImageGenerationService diaryImageGenerationService;
+    private final NotificationService notificationService;
+    private final CommentService commentService;
+
     /**
      * ID로 일기를 조회하여 다른 서비스에서 사용할 수 있도록 반환합니다.
      *
@@ -95,6 +101,23 @@ public class DiaryService {
             }
         }
         log.debug("사용자 [{}] - 사진 저장 완료. 일기 ID: {}", userId, diary.getId());
+
+        if (diary.getStatus() == Status.FRIENDS) {
+            List<String> friends = friendRequestService.getFriends(userId);
+            for (String friendId : friends) {
+
+                if (friendId.equals(userId)) continue;
+
+                notificationService.sendNotification(
+                        friendId,
+                        NotificationType.FRIEND_DIARY,
+                        user.getId(),
+                        diary
+                );
+            }
+            log.info("친구에게 새 일기 공개 알림 전송 완료. 친구 수: {}", friends.size());
+
+        }
         return new ResponseDiaryDTO(
                 diary.getId(),
                 diary.getContent()
@@ -270,57 +293,6 @@ public class DiaryService {
         }
     }
 
-
-    @Transactional(readOnly = true)
-    public ResponseDTO getDiaryWithPhotos(Long diaryId, RequestMetaInfo requestMetaInfo,String user_id) {
-        log.info("{} 일기 내용 조회 요청", diaryId);
-        Diary diary = getDiaryById(diaryId);
-
-        List<Photo> photos = photoRepository.findByDiaryId(diary.getId());
-//        if (photos == null || photos.isEmpty()) {
-//            log.warn("DiaryId {} 에 해당하는 사진이 없음!", diaryId);
-//            throw new DiaryNotFoundException();
-//        }
-
-        List<String> sortedPhotoUrls = sortPhotos(photos,requestMetaInfo);
-        boolean isOwner = diary.getUser().getId().equals(user_id);
-        boolean isFriend = friendRequestService.areFriends(diary.getUser().getId(), user_id);
-
-        String avatarUrl = imagePathToUrlConverter.userAvatarImageUrl(diary.getUser().getAvatar(), requestMetaInfo);
-
-        // 비공개 + 본인 아님 → 대표 사진만 반환
-        if ((diary.getStatus() == Status.PRIVATE && !isOwner)
-                || (diary.getStatus() == Status.FRIENDS && !isOwner && !isFriend)) {
-            return new ResponseDTO(
-                    diary.getId(),
-                    diary.getStatus(),
-                    null,
-                    List.of(sortedPhotoUrls.get(0)),
-                    diary.getDate(),
-                    diary.getUser().getNickname(),
-                    avatarUrl,
-                    diary.getUser().getId(),
-                    diary.getCreatedAt(),
-                    null
-            );
-        }
-
-        // 공개이거나 본인일 경우 대표 사진 포함 전체 사진 리스트 반환
-        return new ResponseDTO(
-                diary.getId(),
-                diary.getStatus(),
-                diary.getContent(),
-                sortedPhotoUrls,
-                diary.getDate(),
-                diary.getUser().getNickname(),
-                avatarUrl,
-                diary.getUser().getId(),
-                diary.getCreatedAt(),
-                null
-        );
-    }
-
-
     public List<CalendarDiaryResponseDTO> findMonthlyDiaries(String userId, int year, int month, RequestMetaInfo requestMetaInfo) {
         YearMonth yearMonth = YearMonth.of(year, month);
         LocalDate startOfMonth = yearMonth.atDay(1);
@@ -338,7 +310,7 @@ public class DiaryService {
     }
 
 
-    private List<String> sortPhotos(List<Photo> photos,RequestMetaInfo requestMetaInfo ) {
+    List<String> sortPhotos(List<Photo> photos, RequestMetaInfo requestMetaInfo) {
         for (int i = 0; i < photos.size(); i++) {
             if (Boolean.TRUE.equals(photos.get(i).getRepresent())) {
                 if (i != 0) {
@@ -375,43 +347,6 @@ public class DiaryService {
         return PageRequest.of(page, size, safeSort);
     }
 
-
-    /**
-     * 공개 상태인 일기들을 페이지네이션과 함께 조회하고,
-     * 각 일기별 대표 사진이 앞에 오도록 사진 URL 리스트를 정렬하여 반환합니다.
-     *
-     * @param pageable 조회할 페이지 번호 (0부터 시작)
-     * @return 공개된 일기 리스트의 DTO를 담은 Page
-     */
-    public Page<ResponseDTO> getAllDiaries(Pageable pageable ,RequestMetaInfo requestMetaInfo,String user_id) {
-
-        List<String> allowed = List.of("createdAt");
-        Pageable safePageable = sanitizePageable(pageable,allowed);
-        List<String> friendIds = friendRequestService.findFriendIdList(safePageable,user_id,requestMetaInfo);
-
-        Page<Diary> page = diaryRepository.findFeedByFriendIdsOrPublic(friendIds, safePageable);
-
-
-        return page.map(diary -> {
-            List<Photo> photos = photoRepository.findByDiaryId(diary.getId());
-            List<String> sortedPhotoUrls = sortPhotos(photos,requestMetaInfo);
-            String avatarUrl = imagePathToUrlConverter.userAvatarImageUrl(diary.getUser().getAvatar(), requestMetaInfo);
-            FriendStatus friendshipStatus = friendRequestService.getFriendshipStatus(user_id, diary.getUser().getId());
-
-            return new ResponseDTO(
-                    diary.getId(),
-                    diary.getStatus(),
-                    diary.getContent(),
-                    sortedPhotoUrls,
-                    diary.getDate(),
-                    diary.getUser().getNickname(),
-                    avatarUrl,
-                    diary.getUser().getId(),
-                    diary.getCreatedAt(),
-                    friendshipStatus
-            );
-        });
-    }
 
     /**
      * 특정 사용자가 작성한 일기의 총 개수를 반환합니다.
