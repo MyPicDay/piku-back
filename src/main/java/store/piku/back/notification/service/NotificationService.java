5306a9f734fc72a36ae8da73e413b14d2c690c7c
apfp77 <apfp77@naver.com>
2025-10-18 17:04:07 +0900

    fix: ERROR dispatcher AccessDeniedException

--- src/main/java/store/piku/back/notification/service/NotificationService.java
+++ src/main/java/store/piku/back/notification/service/NotificationService.java
@@ -104,7 +104,7 @@ public class NotificationService {
         } catch (IOException e) {
             log.info("클라이언트와 연결 끊김, emitter 삭제 요청");
             emitterRepository.deleteById(emitterId);
-            throw new RuntimeException("연결 오류!");
+            // throw new RuntimeException("연결 오류!");
         }
     }
     public void sendToClient(SseEmitter emitter, String eventId, String emitterId, Long count) {
@@ -116,7 +116,7 @@ public class NotificationService {
         } catch (IOException e) {
             log.info("(초기) 클라이언트와 연결 끊김, emitter 삭제 요청");
             emitterRepository.deleteById(emitterId);
-            throw new RuntimeException("연결 오류!");
+            // throw new RuntimeException("연결 오류!");
         }
     }
 
