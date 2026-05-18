84763273a28375110152faf513898494bb11a21b
apfp77 <apfp77@naver.com>
2026-05-18 19:28:02 +0900

    fix: 답글 오탈자 수정

--- src/main/java/com/pikume/back/notification/application/service/NotificationService.java
+++ src/main/java/com/pikume/back/notification/application/service/NotificationService.java
@@ -115,7 +115,7 @@ public class NotificationService implements NotificationUseCase {
 			case FRIEND_REQUEST -> "님이 친구 요청을 보냈습니다";
 			case FRIEND_ACCEPT -> "님이 친구 수락했습니다.";
 			case COMMENT -> "님이 일기에 댓글을 달았습니다.";
-			case REPLY -> "님이 회원님의 댓글에 답글들 달았습니다.";
+			case REPLY -> "님이 회원님의 댓글에 답글을 달았습니다.";
 			case FRIEND_DIARY -> "님이 새 일기를 작성하였습니다.";
 			case LIKE -> "님이 회원님의 일기를 좋아합니다.";
 		};
